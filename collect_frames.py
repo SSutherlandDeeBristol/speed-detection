bec065e9c82fa5633afe2c96d89723070bd32c02
SamSD <ss15060@my.bristol.ac.uk>
2020-02-05 22:48:37 +0000

    Just need to correct orientation tomorrow

--- collect_frames.py
+++ collect_frames.py
@@ -2,48 +2,112 @@ import cv2 as cv
 import json
 import os
 import pickle as pkl
+import skvideo.io
 
-json_dir = 'info/train/'
-vid_dir = 'videos/train/'
+json_dir = '../info/train/'
+vid_dir = '../videos/train/'
+image_dir = '../images/train/'
 
-vid_map = {}
-vid_to_json = {}
-
-# TODO: save the name of the JSONs that have the videos currently in the vid_dir directory
-# TODO: pickle a dict from video filename -> JSON file for easy reading
 if __name__ == '__main__':
-    for filename in os.listdir(vid_dir):
-        if not filename.endswith('.mov'):
-            continue
+    vid_map = {}
+    json_map = {}
+    image_map = {}
 
-        vid_map[filename] = os.path.join(vid_dir, filename)
-
-    pkl.dump(vid_to_json, open('vid_to_json.pkl', 'w'))
+    image_counter = 0
 
     for i, filename in enumerate(os.listdir(json_dir)):
-        if not filename.endswith('.json'):
+        if filename.endswith('.json'):
+            json_map[filename[:-5]] = filename
+
+    for i, filename in enumerate(os.listdir(vid_dir)):
+        if filename.endswith('.mov'):
+            vid_map[filename[:-4]] = filename
+
+    for i, key in enumerate(vid_map.keys()):
+        print(f'Processing {vid_map[key]}')
+        if not key in json_map.keys():
+            print('No JSON file found for video..')
             continue
-
-        # print(f'reading {os.path.join(json_dir, filename)}')
-        print(i)
-
         try:
-            json_obj = json.load(open(os.path.join(json_dir, filename), 'r'))
+            json_obj = json.load(open(os.path.join(json_dir, json_map[key])))
 
-            if json_obj['filename'] is None:
+            if json_obj['gps'] is None or json_obj['startTime'] is None:
+                print('No GPS or startTime data found..')
                 continue
 
-            if json_obj['filename'] in vid_map.keys():
-                print("hooray")
-                vid_to_json[json_obj['filename']] = filename
+            print('Finding speeds and saving frames..')
+
+            start_time = json_obj['startTime']
+
+            vid = skvideo.io.VideoCapture(os.path.join(vid_dir, vid_map[key]))
+
+            if int(vid.get(cv.CAP_PROP_FRAME_HEIGHT)) != 1280 or int(vid.get(cv.CAP_PROP_FRAME_WIDTH) != 720):
+                print('Video not high res..')
+                continue
+
+            for gps_entry in json_obj['gps']:
+                timestamp = gps_entry['timestamp']
+                speed = gps_entry['speed']
+
+                vid.set(cv.CAP_PROP_POS_MSEC, int(timestamp - start_time))
+
+                print(f'Scrubbed to time {timestamp - start_time}..')
+
+                current_frame = vid.get(cv.CAP_PROP_POS_FRAMES)
+
+                print(f'Current frame: {int(current_frame)}')
+
+                print('Reading frame..')
+
+                try:
+                    success, current_image = vid.read()
+
+                except:
+                    print(f'Could not read frame {int(current_frame)}..')
+                    continue
+
+                if not success or current_frame < 1:
+                    print(f'Could not read frame {int(current_frame)}..')
+                    continue
+
+                vid.set(cv.CAP_PROP_POS_FRAMES, current_frame - 1)
+
+                print(f'Scrubbed to frame {int(current_frame - 1)}')
+
+                print('Reading frame..')
+
+                try:
+                    success, prev_image = vid.read()
+                except:
+                    print(f'Could not read frame {int(current_frame - 1)}..')
+                    continue
+
+                if not success:
+                    print(f'Could not read frame {current_frame - 1}..')
+                    continue
+
+                prev_filename = os.path.join(image_dir, f'{image_counter}-prev.jpg')
+                current_filename = os.path.join(image_dir, f'{image_counter}-current.jpg')
+
+                print(f'Writing image to {prev_filename}..')
+                cv.imwrite(os.path.join(image_dir, f'{image_counter}-prev.jpg'), prev_image)
+                print(f'Writing image to {current_filename}..')
+                cv.imwrite(os.path.join(image_dir, f'{image_counter}-current.jpg'), current_image)
+
+                image_map[image_counter] = (f'{image_counter}-prev.jpg', f'{image_counter}-current.jpg', speed)
+
+                image_counter = image_counter + 1
+
+            vid.release()
+
         except:
-            continue
+            print('Problem loading JSON..')
 
-    
+    pkl.dump(image_map, open('image_map.pkl', 'wb'))
 
-        # cap = cv.VideoCapture('videos/train/00a0f008-a315437f.mov')
+    # cap = cv.VideoCapture('videos/train/00a0f008-a315437f.mov')
 
-        # print(f'frame width: {cap.get(cv.CAP_PROP_FRAME_WIDTH)}')
-        # print(f'frame height: {cap.get(cv.CAP_PROP_FRAME_HEIGHT)}')
-        # print(f'fps: {cap.get(cv.CAP_PROP_FPS)}')
-        # print(f'num frames: {cap.get(cv.CAP_PROP_FRAME_COUNT)}')
+    # print(f'frame width: {cap.get(cv.CAP_PROP_FRAME_WIDTH)}')
+    # print(f'frame height: {cap.get(cv.CAP_PROP_FRAME_HEIGHT)}')
+    # print(f'fps: {cap.get(cv.CAP_PROP_FPS)}')
+    # print(f'num frames: {cap.get(cv.CAP_PROP_FRAME_COUNT)}')
