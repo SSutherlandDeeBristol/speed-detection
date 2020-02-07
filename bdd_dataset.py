7b817ef825273bd3e2a5fb9be1b21d8d1240e11c
SamSD <ss15060@my.bristol.ac.uk>
2020-02-07 11:04:02 +0000

    Finished extracting images from 2000 videos

--- bdd_dataset.py
+++ bdd_dataset.py
@@ -1,1 +1,23 @@
 import torch
+import pickle
+import os
+import cv2 as cv
+
+class BDDDataset(data.Dataset):
+    def __init__(self, of_image_path, of_map_path, transforms):
+        self.of_image_path = of_image_path
+        self.transforms = transforms
+        self.of_map = pickle.load(open(of_map_path, 'rb'))
+
+    def __getitem__(self, index):
+        image_name, speed = self.of_map[index]
+
+        image_path = os.path.join(self.of_image_path, image_name)
+
+        image = cv2.imread(image_path)
+
+        return image, speed
+
+    def __len__(self):
+        return len(self.of_map.keys())
+
--- generate_optical_flow.py
+++ generate_optical_flow.py
@@ -2,9 +2,42 @@ import numpy as np
 import cv2 as cv
 import pickle as pkl
 
+of_dir = '../optical-flow/train/'
+image_dir = '../images/train/'
+
 if __name__ == '__main__':
 
     image_map = pkl.load(open('image_map.pkl', 'rb'))
 
+    of_map = {}
+
     for i, (k, (prev, current, speed)) in enumerate(image_map.items()):
         print(f'{i} | {prev} -> {current} | {speed}m/s')
+
+        prev_path = os.path.join(image_dir, prev)
+        current_path = os.path.join(image_dir, current)
+
+        prev_image = cv.imread(prev_path)
+        current_image = cv.imread(current_path)
+
+        prev_gray = cv.cvtColor(prev_image, cv.COLOR_BGR2GRAY)
+        current_gray = cv.cvtColor(current_image, cv.COLOR_BGR2GRAY)
+
+        mask = np.zeros_like(first_frame)
+        mask[..., 1] = 255
+
+        flow = cv.calcOpticalFlowFarneback(prev_gray, gray, None, 0.5, 1, 20, 5, 5, 1.1, 0)
+
+        magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1])
+
+        mask[..., 0] = angle * 180 / np.pi / 2
+
+        mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
+
+        rgb = cv.cvtColor(mask, cv.COLOR_HSV2BGR)
+
+        cv.imwrite(open(os.path.join(of_dir, f'{k}.jpg'), 'wb'), rgb)
+
+        of_map[f'{k}.jpg'] = speed
+
+    pkl.dump(of_map, open('optical_flow_map.pkl', 'wb'))
