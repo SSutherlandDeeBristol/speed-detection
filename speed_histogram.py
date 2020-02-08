ed06882dd978ae4b4fd6fbd3084480f41636bd88
SamSD <ss15060@my.bristol.ac.uk>
2020-02-08 13:59:53 +0000

    Updated speed_histogram

--- speed_histogram.py
+++ speed_histogram.py
@@ -1,10 +1,11 @@
 import pickle as pkl
 import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__=='__main__':
 
-    of_map = pkl.load(open('optical_flow_map.pkl', 'rb'))
+    of_map = pkl.load(open('../optical-flow/train/optical_flow_map.pkl', 'rb'))
 
-    plt.hist(of_map.values(), bins='auto')
+    plt.hist(np.concatenate([[s for (_,s) in speeds] for speeds in of_map.values()]), bins='auto')
     plt.title('Histogram of speeds in the training set.')
     plt.show()
