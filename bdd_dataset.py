1e2fc837c21c3e6ffba85da7548584f77235c0f4
SamSD <ss15060@my.bristol.ac.uk>
2020-02-11 15:30:29 +0000

    Fixed transform

--- bdd_dataset.py
+++ bdd_dataset.py
@@ -22,7 +22,8 @@ class BDDDataset(data.Dataset):
 
         image = np.transpose(image, (2,1,0))
 
-        image = transforms(image)
+        if self.transforms:
+            image = self.transforms(image)
 
         print("Loaded image..")
 
