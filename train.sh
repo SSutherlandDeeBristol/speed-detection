0f04d635f8b099f65378477a1c6711e33779d7d8
SamSD <ss15060@my.bristol.ac.uk>
2020-02-12 16:07:01 +0000

    Added bash script

---
+++ train.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#SBATCH --partition gpu
+#SBATCH --time 0-05:00:00
+#SBATCH --mem 64GB
+#SBATCH --gres gpu:1
+
+module load "languages/anaconda3/2019.07-3.6.5-tflow-1.14"
+
+python3 train.py
