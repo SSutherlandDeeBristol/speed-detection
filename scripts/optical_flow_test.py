9420eed45e978aff3144fd2125f4648064c802d5
SamSD <ss15060@my.bristol.ac.uk>
2020-02-20 11:50:38 +0000

    Testing BGR vs RGB conversion

--- scripts/optical_flow_test.py
+++ scripts/optical_flow_test.py
@@ -14,16 +14,28 @@ if __name__ == '__main__':
     current_gray = cv.cvtColor(current_image, cv.COLOR_BGR2GRAY)
 
     mask = np.zeros_like(current_image)
-    mask[..., 1] = 255
 
-    flow = cv.calcOpticalFlowFarneback(prev_gray, current_gray, None, 0.5, 1, 10, 10, 5, 1.1, 0)
+    hsv_current_rgb = cv.cvtColor(current_image, cv.COLOR_RGB2HSV)
+    hsv_current_bgr = cv.cvtColor(current_image, cv.COLOR_BGR2HSV)
+
+    mask_rgb[:,:,1] = hsv_current_rgb[:,:,1]
+    mask_bgr[:,:,1] = hsv_current_bgr[:,:,1]
+
+    flow = cv.calcOpticalFlowFarneback(prev_gray, current_gray, None, 0.5, 1, 15, 2, 5, 1.3, 0)
 
     magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1])
 
-    mask[..., 0] = angle * 180 / np.pi / 2
+    mask_rgb[..., 0] = angle * (180 / np.pi / 2)
+    mask_bgr[..., 0] = angle * (180 / np.pi / 2)
 
-    mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
+    mask_rgb[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
+    mask_bgr[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
 
-    rgb = cv.cvtColor(mask, cv.COLOR_HSV2BGR)
+    mask_rgb = np.asarray(mask, dtype=np.float32)
+    mask_bgr = np.asarray(mask, dtype=np.float32)
 
-    cv.imwrite('optical_flow_test.jpg', rgb)
+    rgb = cv.cvtColor(mask_rgb, cv.COLOR_HSV2RGB)
+    bgr = cv.cvtColor(mask_bgr, cv.COLOR_HSV2BGR)
+
+    cv.imwrite(os.path.join(f'test_rgb.png'), rgb)
+    cv.imwrite(os.path.join(f'test_bgr.png'), bgr)
