74535545d7f5e0ce852fad459cfbc4c8fa569dc0
SamSD <ss15060@my.bristol.ac.uk>
2020-02-25 17:18:12 +0000

    dropout fc1

--- network/cnn.py
+++ network/cnn.py
@@ -13,6 +13,7 @@ class CNN(nn.Module):
 
         self.dropout1 = nn.Dropout(0.5)
         self.dropout2 = nn.Dropout(0.5)
+        self.dropout3 = nn.Dropout(0.5)
 
         self.conv1 = nn.Conv2d(
             in_channels=self.input_channels,
@@ -114,6 +115,8 @@ class CNN(nn.Module):
         x = self.dropout2(x)
 
         x = F.relu(self.fc1(x))
+
+        x = self.dropout3(x)
 
         x = F.relu(self.fc2(x))
 
