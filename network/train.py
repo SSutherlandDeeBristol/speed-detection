212c8839405c2da41fc2a7a99a6ced37f642d367
SamSD <ss15060@my.bristol.ac.uk>
2020-03-22 16:12:02 +0000

    Changed sum to mean for loss function

--- network/train.py
+++ network/train.py
@@ -56,7 +56,7 @@ parser.add_argument('--bs',
 
 def custom_loss(output, target):
     x = output.sub(target)
-    pos_mask = x.ge(0)
+    pos_mask = x.gt(0)
     neg_mask = x.lt(0)
 
     pos_error = torch.masked_select(x, pos_mask)
@@ -67,7 +67,7 @@ def custom_loss(output, target):
 
     errors = torch.cat(pos_error, neg_error)
 
-    loss = torch.sum(errors)
+    loss = torch.mean(errors)
 
     return loss
 
