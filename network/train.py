c60dd720e4856228944649233d0a8eb963118619
SamSD <ss15060@my.bristol.ac.uk>
2020-03-25 21:41:02 +0000

    Truncated loss -10,10

--- network/train.py
+++ network/train.py
@@ -65,7 +65,7 @@ def truncated_sum(output, target):
 
 def truncated_mse(output, target):
     x = output.sub(target)
-    x = torch.clamp(x, -8, 8)
+    x = torch.clamp(x, -10, 10)
     x = x.pow(2)
 
     loss = torch.mean(x)
