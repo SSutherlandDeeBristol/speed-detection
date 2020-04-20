10d18888c6780a6747c7470872699e67e1bc0df3
SamSD <ss15060@my.bristol.ac.uk>
2020-04-21 00:07:27 +0100

    Back to good model, get a run with normalised images

--- network/cnn.py
+++ network/cnn.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 from torch import nn
 from torch.nn import functional as F
+import math
 
 class CNN(nn.Module):
     def __init__(self, image_width, image_height, input_channels):
@@ -21,6 +22,7 @@ class CNN(nn.Module):
             bias=False
         )
         self.initialise_layer(self.conv1)
+        self.norm1 = nn.BatchNorm2d(24)
 
         self.conv2 = nn.Conv2d(
             in_channels=self.conv1.out_channels,
@@ -31,38 +33,47 @@ class CNN(nn.Module):
             bias=False
         )
         self.initialise_layer(self.conv2)
+        self.norm2 = nn.BatchNorm2d(36)
 
         self.conv3 = nn.Conv2d(
             in_channels=self.conv2.out_channels,
             out_channels=48,
             kernel_size=(5, 5),
             padding=(2, 2),
-            stride=(2,2),
+            stride=(2, 2),
             bias=False
         )
         self.initialise_layer(self.conv3)
+        self.norm3 = nn.BatchNorm2d(48)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=(2,2), stride=(2,2), padding=1)
 
         self.conv4 = nn.Conv2d(
             in_channels=self.conv3.out_channels,
             out_channels=64,
             kernel_size=(3, 3),
             padding=(1, 1),
+            stride=(1, 1),
             bias=False
         )
         self.initialise_layer(self.conv4)
+        self.norm4 = nn.BatchNorm2d(64)
 
         self.conv5 = nn.Conv2d(
             in_channels=self.conv4.out_channels,
             out_channels=64,
             kernel_size=(3, 3),
             padding=(1, 1),
+            stride=(1, 1),
             bias=False
         )
         self.initialise_layer(self.conv5)
+        self.norm5 = nn.BatchNorm2d(64)
 
-        size = int((image_height / 8) * (image_width / 8) * self.conv5.out_channels)
+        # size = int((math.floor(image_height/32) + 1) * (math.floor(image_width/32) + 1)) * self.conv5.out_channels
 
-        self.fc1 = nn.Linear(size, 1164)
+        self.fc1 = nn.Linear(60352, 1164)
+        # self.fc1 = nn.Linear(120704, 1164)
         self.initialise_layer(self.fc1)
 
         self.fc2 = nn.Linear(1164, 100)
@@ -77,30 +88,36 @@ class CNN(nn.Module):
         self.fc5 = nn.Linear(10, 1)
         self.initialise_layer(self.fc5)
 
+        self.tan = nn.Tanh()
+
     def forward(self, images) -> torch.Tensor:
-        x = F.elu(self.conv1(images))
+        x = F.relu(self.norm1(self.conv1(images)))
 
-        x = F.elu(self.conv2(x))
+        x = F.relu(self.norm2(self.conv2(x)))
 
-        x = F.elu(self.conv3(x))
+        x = F.relu(self.norm3(self.conv3(x)))
 
-        # x = self.dropout(x)
+        x = self.pool1(x)
 
-        x = F.elu(self.conv4(x))
+        x = F.relu(self.norm4(self.conv4(x)))
 
-        x = F.elu(self.conv5(x))
+        x = F.relu(self.norm5(self.conv5(x)))
 
         x = torch.flatten(x, start_dim=1)
 
-        x = F.elu(self.fc1(x))
+        x = self.dropout(x)
 
-        x = F.elu(self.fc2(x))
+        x = F.relu(self.fc1(x))
 
-        x = F.elu(self.fc3(x))
+        x = F.relu(self.fc2(x))
 
-        x = F.elu(self.fc4(x))
+        x = F.relu(self.fc3(x))
+
+        x = F.relu(self.fc4(x))
 
         x = self.fc5(x)
+
+        x = self.tan(x/100) * 45.0
 
         return x
 
@@ -110,4 +127,3 @@ class CNN(nn.Module):
             nn.init.zeros_(layer.bias)
         if hasattr(layer, "weight"):
             nn.init.kaiming_normal_(layer.weight)
-
