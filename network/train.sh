43cd5ac0c35354c0012682fe3a9db8a97eb1203e
SamSD <ss15060@my.bristol.ac.uk>
2020-04-21 16:40:02 +0100

    Longer run time for BS 32

--- network/train.sh
+++ network/train.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #SBATCH --partition gpu
-#SBATCH --time 0-08:00:00
+#SBATCH --time 0-11:00:00
 #SBATCH --mem 64GB
 #SBATCH --gres gpu:1
 
 module load "languages/anaconda3/2019.07-3.6.5-tflow-1.14"
 
-python3 train.py --lr=0.001 --bs=128
+python3 train.py --lr=0.001 --bs=32
