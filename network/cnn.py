3ebdc1325f1c462d46e1cbc0a369b6786250572a
SamSD <ss15060@my.bristol.ac.uk>
2020-04-24 14:35:19 +0100

    No Data Aug

--- network/cnn.py
+++ network/cnn.py
@@ -91,29 +91,29 @@ class CNN(nn.Module):
         self.tan = nn.Tanh()
 
     def forward(self, images) -> torch.Tensor:
-        x = F.rrelu(self.norm1(self.conv1(images)))
+        x = F.relu(self.norm1(self.conv1(images)))
 
-        x = F.rrelu(self.norm2(self.conv2(x)))
+        x = F.relu(self.norm2(self.conv2(x)))
 
-        x = F.rrelu(self.norm3(self.conv3(x)))
+        x = F.relu(self.norm3(self.conv3(x)))
 
         x = self.pool1(x)
 
-        x = F.rrelu(self.norm4(self.conv4(x)))
+        x = F.relu(self.norm4(self.conv4(x)))
 
-        x = F.rrelu(self.norm5(self.conv5(x)))
+        x = F.relu(self.norm5(self.conv5(x)))
 
         x = torch.flatten(x, start_dim=1)
 
         x = self.dropout(x)
 
-        x = F.rrelu(self.fc1(x))
+        x = F.relu(self.fc1(x))
 
-        x = F.rrelu(self.fc2(x))
+        x = F.relu(self.fc2(x))
 
-        x = F.rrelu(self.fc3(x))
+        x = F.relu(self.fc3(x))
 
-        x = F.rrelu(self.fc4(x))
+        x = F.relu(self.fc4(x))
 
         x = self.fc5(x)
 
